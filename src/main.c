2010179087bc74f8d0d6f5e0e409c7247d5844a5
Quantum Soul <emericdescoings@yahoo.fr>
2017-08-29 13:05:06 +0200

    màj

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: edescoin <edescoin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/29 12:53:37 by edescoin          #+#    #+#             */
-/*   Updated: 2017/08/29 12:57:22 by edescoin         ###   ########.fr       */
+/*   Updated: 2017/08/29 13:04:50 by edescoin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ void	init_list_evts(t_event **head, t_evt_data *data)
 	new_event(head, SDL_QUIT, NULL, &force_exit);
 }
 
-int	main(int ac, char **av)
+int		main(int ac, char **av)
 {
 	t_event		*events;
 
