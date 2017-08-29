d374f6d70cc98e522a2406b953da643162888747
Quantum Soul <emericdescoings@yahoo.fr>
2017-08-29 13:08:52 +0200

    màj

--- src/main.c
+++ src/main.c
@@ -6,25 +6,25 @@
 /*   By: edescoin <edescoin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/29 12:53:37 by edescoin          #+#    #+#             */
-/*   Updated: 2017/08/29 13:04:50 by edescoin         ###   ########.fr       */
+/*   Updated: 2017/08/29 13:08:46 by edescoin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rt.h"
 
-int		force_exit(void)
+static int	force_exit(void)
 {
 	exit(1);
 	return (0);
 }
 
-void	init_list_evts(t_event **head, t_evt_data *data)
+static void	init_list_evts(t_event **head, t_evt_data *data)
 {
 	new_event(head, SDL_KEYUP, data, &key_management);
 	new_event(head, SDL_QUIT, NULL, &force_exit);
 }
 
-int		main(int ac, char **av)
+int			main(int ac, char **av)
 {
 	t_event		*events;
 
