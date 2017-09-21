90f201c1878e0789dc2adbf0a19fb2fc1bbfe53f
Quantum Soul <emericdescoings@yahoo.fr>
2017-09-21 20:21:27 +0200

    modif du constructeur de la caméra pour l'initialisation du screen

--- src/camera.c
+++ src/camera.c
@@ -6,40 +6,33 @@
 /*   By: edescoin <edescoin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/21 13:12:56 by edescoin          #+#    #+#             */
-/*   Updated: 2017/09/21 20:13:42 by edescoin         ###   ########.fr       */
+/*   Updated: 2017/09/21 20:19:32 by edescoin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rt.h"
 #include <math.h>
-/*
-void		init_cam_screen(t_camera *cam)
+
+static void	init_cam_screen(t_camera *cam)
 {
 	t_dot		tmp;
-	t_matrix	*transfo;
 	int			i;
 	int			j;
 
-	transfo = create_identity(4);
-	translation(&transfo, cam->crd.x, cam->crd.y, cam->crd.z);
-	y_rotation(&transfo, cam->theta);
-	x_rotation(&transfo, cam->phi);
 	tmp.x = -WIN_WIDTH / 2;
-	i = 0;
-	while ((i += cam->resolution) < WIN_WIDTH)
+	i = -1;
+	while (++i < WIN_WIDTH)
 	{
-		tmp.x += cam->resolution;
+		tmp.x++;
 		tmp.y = -WIN_HEIGHT / 2;
-		j = 0;
-		while ((j += cam->resolution) < WIN_HEIGHT)
+		j = -1;
+		while (++j < WIN_HEIGHT)
 		{
-			tmp.y += cam->resolution;
-			set_dot(&cam->screen[i][j], tmp.x, tmp.y, cam->f);
-			mult_vect((t_vector*)&cam->screen[i][j], transfo,
-						(t_vector*)&cam->screen[i][j]);
+			tmp.y++;
+			cam->screen[i][j] = (t_dot){tmp.x, tmp.y, cam->focal};
 		}
 	}
-}*/
+}
 
 t_camera	*new_camera(int fov, const t_dot origin,
 						double angle_x, double angle_y, double angle_z)
@@ -53,7 +46,7 @@ t_camera	*new_camera(int fov, const t_dot origin,
 	cam->angle_y = angle_y;
 	cam->angle_z = angle_z;
 	cam->focal = WIN_WIDTH / (2 * tan(ft_to_rad(fov) / 2.0f));
-	//init_cam_screen(cam);
+	init_cam_screen(cam);
 	return (cam);
 }
 
