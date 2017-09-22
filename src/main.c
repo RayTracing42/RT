2f06fd4b53f6d1b7b0c1f5d95c9957bba9751308
Quantum Soul <emericdescoings@yahoo.fr>
2017-09-22 13:49:01 +0200

    ajout de trucs dans la scène + correction light

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: edescoin <edescoin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/29 12:53:37 by edescoin          #+#    #+#             */
-/*   Updated: 2017/09/22 13:27:04 by edescoin         ###   ########.fr       */
+/*   Updated: 2017/09/22 13:47:36 by edescoin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,8 +36,9 @@ int			main(int ac, char **av)
 
 
 	t_scene	*scene = new_scene(new_camera(60, (t_dot){0, 0, 0}, 0, 0, 0), 100);
-	scene_add_light(new_light(ORB, (t_vector){0, 0, 0}, (SDL_Color){255, 255, 255, 255}), scene);
+	scene_add_light((t_light*)new_orb_light((t_dot){10, 10, 10}, (SDL_Color){255, 255, 255, 255}), scene);
 	scene_add_object((t_object*)new_sphere((t_objs_comp){(t_dot){10, 0, 0}, (t_vector){0, 0, 0}, (SDL_Color){255, 255, 255, 255}}, 3), scene);
+	scene_add_object((t_object*)new_sphere((t_objs_comp){(t_dot){10, 20, 0}, (t_vector){0, 0, 0}, (SDL_Color){255, 255, 255, 255}}, 5), scene);
 
 
 	wait_events(events);
