aa22bd60ca833ae593be5534b1473cc59a178c20
Quantum Soul <emericdescoings@yahoo.fr>
2017-09-22 13:13:28 +0200

    ajout de la gestions des lights de la scène + organisation

--- include/scene.h
+++ include/scene.h
@@ -6,13 +6,15 @@
 /*   By: edescoin <edescoin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/06 17:10:39 by edescoin          #+#    #+#             */
-/*   Updated: 2017/09/22 13:05:02 by edescoin         ###   ########.fr       */
+/*   Updated: 2017/09/22 13:12:13 by edescoin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SCENE_H
 # define SCENE_H
 # include "structures.h"
+# include "scene_objs.h"
+# include "scene_lights.h"
 
 /*
 ** scene.c
--- include/scene_lights.h
+++ include/scene_lights.h
@@ -1,38 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   rt.h                                               :+:      :+:    :+:   */
+/*   scene_lights.h                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: edescoin <edescoin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/08/25 10:49:54 by edescoin          #+#    #+#             */
-/*   Updated: 2017/09/22 13:12:14 by edescoin         ###   ########.fr       */
+/*   Created: 2017/09/22 13:04:14 by edescoin          #+#    #+#             */
+/*   Updated: 2017/09/22 13:11:32 by edescoin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef RT_H
-# define RT_H
-# ifndef __APPLE__
-#  include <SDL2/SDL.h>
-#  define SDL_VIDEO		SDL_INIT_VIDEO
-# else
-#  include "SDL2/SDL.h"
-#  define SDL_VIDEO		SDL_VIDEO_DRIVER_COCOA
-# endif
-
-# define TITLE			"RT"
-
-# include <stdlib.h>
-# include <stdio.h>
-# include <unistd.h>
-# include "camera.h"
-# include "events.h"
-# include "graphics.h"
-# include "libft.h"
-# include "lights.h"
-# include "objects.h"
-# include "scene.h"
+#ifndef SCENE_LIGHTS_H
+# define SCENE_LIGHTS_H
 # include "structures.h"
 
+void	delete_cell_light(t_list_lights **cell);
+void	scene_add_light(t_light *light, t_scene *scene);
 
 #endif
