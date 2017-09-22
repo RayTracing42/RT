a15698e6be74d10a0f16b9c4f4fd0ca20cbc0cef
Quantum Soul <emericdescoings@yahoo.fr>
2017-09-22 14:15:11 +0200

    suppr fcts inutiles

--- include/vectors.h
+++ include/vectors.h
@@ -6,7 +6,7 @@
 /*   By: edescoin <edescoin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/11 12:52:00 by edescoin          #+#    #+#             */
-/*   Updated: 2017/09/11 12:52:41 by edescoin         ###   ########.fr       */
+/*   Updated: 2017/09/22 14:13:51 by edescoin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,6 @@
 # define VECTORS_H
 # include "structures.h"
 
-t_vector	*new_vector(double x, double y, double z);
-void		set_vector(t_vector *vect, double x, double y, double z);
 double		get_vect_lenght(const t_vector *vect);
 double		vect_dot_product(const t_vector *v1, const t_vector *v2);
 
--- src/vectors.c
+++ src/vectors.c
@@ -6,31 +6,12 @@
 /*   By: edescoin <edescoin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/14 14:45:00 by edescoin          #+#    #+#             */
-/*   Updated: 2017/09/11 12:51:40 by edescoin         ###   ########.fr       */
+/*   Updated: 2017/09/22 14:14:08 by edescoin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rt.h"
 #include <math.h>
-
-t_vector	*new_vector(double x, double y, double z)
-{
-	t_vector	*vect;
-
-	if (!(vect = malloc(sizeof(t_dot))))
-		return (NULL);
-	vect->x = x;
-	vect->y = y;
-	vect->z = z;
-	return (vect);
-}
-
-void		set_vector(t_vector *vect, double x, double y, double z)
-{
-	vect->x = x;
-	vect->y = y;
-	vect->z = z;
-}
 
 double		get_vect_lenght(const t_vector *vect)
 {
