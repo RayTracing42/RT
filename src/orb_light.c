28a7595c7397f1ecb1aba6cd07e3dd26bb27a67e
Quantum Soul <emericdescoings@yahoo.fr>
2017-10-03 15:00:22 +0200

    correction caca get_orb_ray_vect

--- src/orb_light.c
+++ src/orb_light.c
@@ -6,7 +6,7 @@
 /*   By: edescoin <edescoin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/25 14:32:44 by edescoin          #+#    #+#             */
-/*   Updated: 2017/09/29 17:48:10 by edescoin         ###   ########.fr       */
+/*   Updated: 2017/10/03 15:00:04 by edescoin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,8 @@ static int	is_in_orb_light(t_light *light, t_ray *light_ray)
 static t_vector	get_orb_ray_vect(t_dot *pos, t_light *light)
 {
 	return ((t_vector){((t_orb_light*)light)->orig.x - pos->x,
-						((t_orb_light*)light)->orig.x - pos->y,
-						((t_orb_light*)light)->orig.x - pos->z});
+						((t_orb_light*)light)->orig.y - pos->y,
+						((t_orb_light*)light)->orig.z - pos->z});
 }
 
 t_orb_light	*new_orb_light(t_dot orig, SDL_Color color)
