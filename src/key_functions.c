c8325e8ba804ccd4e9bcee5f9ffe44131f8beb22
Quantum Soul <emericdescoings@yahoo.fr>
2017-10-25 14:42:58 +0200

    test

--- src/key_functions.c
+++ src/key_functions.c
@@ -6,7 +6,7 @@
 /*   By: edescoin <edescoin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/25 14:48:19 by edescoin          #+#    #+#             */
-/*   Updated: 2017/08/29 12:49:48 by edescoin         ###   ########.fr       */
+/*   Updated: 2017/10/25 14:41:44 by edescoin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ int			key_management(SDL_Event *current, t_event *evt)
 	t_evt_data	*data;
 
 	data = evt->data;
+	(void)data;
 	if (current->key.keysym.sym == SDLK_ESCAPE)
 		return (0);
 	return (1);
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: edescoin <edescoin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/29 12:53:37 by edescoin          #+#    #+#             */
-/*   Updated: 2017/08/29 13:09:35 by edescoin         ###   ########.fr       */
+/*   Updated: 2017/10/25 14:40:48 by edescoin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,8 @@ int			main(int ac, char **av)
 	events = NULL;
 	get_sdl_core();
 	init_list_evts(&events, NULL);
+	t_scene *sc = new_scene(NULL, 50);
+	scene_add_light((t_light*)new_orb_light((t_dot){0,0,0}, (SDL_Color){0,0,0,0}), sc);
 	wait_events(events);
 	delete_sdl_core();
 	exit(0);
