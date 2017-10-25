c53af53a16fd5fbd32d576011faa47b92be22092
Emeric DESCOINGS <emericdescoings@yahoo.fr>
2017-10-25 16:54:05 +0200

    màj

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: edescoin <edescoin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/29 12:53:37 by edescoin          #+#    #+#             */
-/*   Updated: 2017/10/25 15:31:17 by edescoin         ###   ########.fr       */
+/*   Updated: 2017/10/25 16:53:53 by edescoin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,8 +33,6 @@ int			main(int ac, char **av)
 	events = NULL;
 	get_sdl_core();
 	init_list_evts(&events, NULL);
-	t_scene *sc = new_scene(NULL, 50);
-	scene_add_light((t_light*)new_spotlight((t_vector){0,0,0}, (t_dot){0,0,0}, (SDL_Color){0,0,0,0}, 360), sc);
 	wait_events(events);
 	delete_sdl_core();
 	exit(0);
