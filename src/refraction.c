bb86e1e53643d725a30bcbfa435876fab091b334
Quantum Soul <emericdescoings@yahoo.fr>
2017-12-17 18:02:24 +0100

    mise à la norme + ajout des tests

--- src/refraction.c
+++ src/refraction.c
@@ -51,12 +51,51 @@ void	get_refracted_col(t_ray *ray, t_object *src,
 			(refracted_obj_col.b * src->obj_light.refraction_amount);
 }
 
+static void	update_tree_in(t_ray *ray, t_ray *ref_ray)
+{
+	int		t_ref;
+	double	n1;
+	double	n2;
+
+	n1 = ray->tree->obj ? ray->tree->obj->obj_light.refractive_index : 1;
+	n2 = ray->obj->obj_light.refractive_index;
+	if (!(t_ref = get_refracted_vect(&ref_ray->equ.vd, &ray->normal, n1, n2)))
+		ref_ray->tree = add_new_leaf(ray->tree, &ray->tree->refracted, ray->obj, ray->tree->lvl + 1);
+	else
+		ref_ray->tree = add_new_leaf(ray->tree, &ray->tree->refracted, ray->tree->obj, ray->tree->lvl);
+}
+
+static int	update_tree_out(t_ray *ray, t_ray *ref_ray)
+{
+	t_objs_tree	*first;
+	int			t_ref;
+	double		n1;
+	double		n2;
+
+	n1 = ray->tree->obj ? ray->tree->obj->obj_light.refractive_index : 1;
+	n2 = ray->obj->obj_light.refractive_index;
+	if (!(first = goto_root_obj(ray->tree, ray->obj)))
+		first = goto_root_obj(ray->tree, ray->tree->obj);
+	if (!first->root)
+		return (0);
+	if (first->lvl >= ray->tree->lvl)
+		n2 = first->root->obj ? first->root->obj->obj_light.refractive_index : 1;
+	if (!(t_ref = get_refracted_vect(&ref_ray->equ.vd, &ray->normal, n1, n2)))
+	{
+		if (first->lvl < ray->tree->lvl)
+			ref_ray->tree = add_new_leaf(first->root, NULL, ray->tree->obj, ray->tree->lvl);
+		else
+			ref_ray->tree = add_new_leaf(first->root, NULL, first->root->obj, first->root->lvl);
+	}
+	else
+		ref_ray->tree = add_new_leaf(ray->tree, &ray->tree->refracted, ray->obj, ray->tree->lvl);
+	return (1);
+}
+
 SDL_Color	refract(t_ray *ray, t_scene *scn)
 {
-	t_ray			ref_ray;
-	t_objs_tree		*first;
-	SDL_Color		ret;
-	int				total_ref;
+	t_ray		ref_ray;
+	SDL_Color	ret;
 
 	ref_ray = *ray;
 	if (ray->obj->obj_light.refraction_amount == 0)
@@ -65,28 +104,9 @@ SDL_Color	refract(t_ray *ray, t_scene *scn)
 		return ((SDL_Color){0, 0, 0, 255});
 	ref_ray.limit -= (1 - ray->obj->obj_light.refraction_amount) / 100;
 	if (ray->nb_intersect == 2)
-	{
-		if (!(total_ref = get_refracted_vect(&ref_ray.equ.vd, &ray->normal, ray->tree->obj ?  ray->tree->obj->obj_light.refractive_index : 1, ray->obj ? ray->obj->obj_light.refractive_index : 1)))
-			ref_ray.tree = add_new_leaf(ray->tree, &ray->tree->refracted, ray->obj, ray->tree->lvl + 1);
-		else
-			ref_ray.tree = add_new_leaf(ray->tree, &ray->tree->refracted, ray->tree->obj, ray->tree->lvl);
-	}
-	else
-	{
-		if (!(first = goto_root_obj(ray->tree, ray->obj)))
-			first = goto_root_obj(ray->tree, ray->tree->obj);
-		if (!first->root)
+		update_tree_in(ray, &ref_ray);
+	else if (!(update_tree_out(ray, &ref_ray)))
 			return ((SDL_Color){0, 0, 0, 255});
-		if (first->lvl < ray->tree->lvl)
-			total_ref = get_refracted_vect(&ref_ray.equ.vd, &ray->normal, ray->tree->obj ?  ray->tree->obj->obj_light.refractive_index : 1, ray->obj ? ray->obj->obj_light.refractive_index : 1);
-		else
-			total_ref = get_refracted_vect(&ref_ray.equ.vd, &ray->normal, ray->tree->obj ? ray->tree->obj->obj_light.refractive_index : 1, (first->root && first->root->obj) ? first->root->obj->obj_light.refractive_index : 1);
-
-		if (!total_ref)
-			ref_ray.tree = add_new_leaf(first->root ? first->root : first, NULL, first->lvl < ray->tree->lvl ? ray->tree->obj : first->root->obj, first->lvl < ray->tree->lvl ? ray->tree->lvl : first->root->lvl);
-		else
-			ref_ray.tree = add_new_leaf(ray->tree, &ray->tree->refracted, ray->obj, ray->tree->lvl);
-	}
 	ref_ray.equ.vc = vector(ray->inter.x + (1 / POW) * ray->equ.vd.x,
 			ray->inter.y + (1 / POW) * ray->equ.vd.y,
 			ray->inter.z + (1 / POW) * ray->equ.vd.z);
