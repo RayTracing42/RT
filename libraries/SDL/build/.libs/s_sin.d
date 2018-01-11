0a807e77750bf8b6968e5595461c2bf9bfce8e22
emeric <emericdescoings@yahoo.fr>
2018-01-11 18:27:49 +0100

    màj makefile

---
+++ libraries/SDL/build/.libs/s_sin.d
@@ -0,0 +1,10 @@
+build/s_sin.lo: \
+  /Users/edescoin/Documents/GitHub/RT/libraries/SDL/src/libm/s_sin.c \
+  /Users/edescoin/Documents/GitHub/RT/libraries/SDL/src/libm/math_libm.h \
+  /Users/edescoin/Documents/GitHub/RT/libraries/SDL/src/libm/../SDL_internal.h \
+  /Users/edescoin/Documents/GitHub/RT/libraries/SDL/src/libm/../dynapi/SDL_dynapi.h \
+  /Users/edescoin/Documents/GitHub/RT/libraries/SDL/src/libm/../dynapi/SDL_dynapi_overrides.h \
+  include/SDL_config.h include/SDL_platform.h include/begin_code.h \
+  include/close_code.h \
+  /Users/edescoin/Documents/GitHub/RT/libraries/SDL/src/libm/math_private.h \
+  include/SDL_endian.h include/SDL_stdinc.h
---
+++ libraries/SDL/build/SDL_dummy_main.d
@@ -0,0 +1,7 @@
+build/SDL_dummy_main.lo: \
+  /Users/edescoin/Documents/GitHub/RT/libraries/SDL/src/main/dummy/SDL_dummy_main.c \
+  /Users/edescoin/Documents/GitHub/RT/libraries/SDL/src/main/dummy/../../SDL_internal.h \
+  /Users/edescoin/Documents/GitHub/RT/libraries/SDL/src/main/dummy/../../dynapi/SDL_dynapi.h \
+  /Users/edescoin/Documents/GitHub/RT/libraries/SDL/src/main/dummy/../../dynapi/SDL_dynapi_overrides.h \
+  include/SDL_config.h include/SDL_platform.h include/begin_code.h \
+  include/close_code.h include/SDL_main.h include/SDL_stdinc.h
