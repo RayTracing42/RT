70e9e6b9e228cdadb34d01f4f4fdf20a866c77d3
Quantum Soul <emericdescoings@yahoo.fr>
2018-01-15 14:16:59 +0100

    fix calcul limites (pb map 8-limit)

--- src/limit.c
+++ src/limit.c
@@ -6,7 +6,7 @@
 /*   By: fcecilie <fcecilie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/22 10:27:56 by fcecilie          #+#    #+#             */
-/*   Updated: 2018/01/15 13:10:27 by shiro            ###   ########.fr       */
+/*   Updated: 2018/01/15 14:14:10 by shiro            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,7 +75,6 @@ static t_ray	check_limit_intersect(t_ray *ray, t_object *father, double *dist, i
 
 	l = father->limit;
 	res_ray = *ray;
-	*dist = 0;
 	while (l)
 	{
 		p = (t_plane *)l->obj;
@@ -99,13 +98,21 @@ static t_ray	check_limit_intersect(t_ray *ray, t_object *father, double *dist, i
 
 void	limit(t_ray *ray, t_ray tmp_ray, const double tmp, double *dist, int i)
 {
-	t_object *obj;
+	t_object	*obj;
+	double		tmp_dist;
 
 	obj = tmp_ray.obj;
+	tmp_dist = *dist;
 	tmp_ray.limit_status = NONE;
 	if (is_in_limit(&tmp_ray, obj))
 		*dist = tmp;
 	else
-		tmp_ray = check_limit_intersect(ray, obj, dist, i);
+	{
+		tmp_ray = check_limit_intersect(ray, obj, &tmp_dist, i);
+		if (!eq(tmp_dist, *dist))
+			*dist = tmp_dist;
+		else
+			*dist = 0;
+	}
 	*ray = tmp_ray;
 }
