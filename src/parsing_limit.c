78b3cdec048cdfe30fa7c1c11e799bb09b537308
Quantum Soul <emericdescoings@yahoo.fr>
2018-02-12 11:20:44 +0100

    simplification système de coordonnées des limites locales

--- src/parsing_limit.c
+++ src/parsing_limit.c
@@ -6,7 +6,7 @@
 /*   By: fcecilie <fcecilie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/29 01:57:03 by fcecilie          #+#    #+#             */
-/*   Updated: 2018/02/11 13:42:20 by shiro            ###   ########.fr       */
+/*   Updated: 2018/02/11 14:18:21 by shiro            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,8 +31,8 @@ void	parsing_local_limit(t_object *o, t_dot origin, t_vector normal,
 {
 	t_plane	*p;
 
-	origin = (t_dot){origin.x - o->origin.x, origin.y - o->origin.y,
-		origin.z - o->origin.z};
+	/*origin = (t_dot){origin.x - o->origin.x, origin.y - o->origin.y,
+		origin.z - o->origin.z};*/
 	mult_vect((t_vector*)&origin, o->trans_const, (t_vector*)&origin);
 	mult_vect(&normal, o->trans_norm, &normal);
 	origin.x += o->origin.x;
