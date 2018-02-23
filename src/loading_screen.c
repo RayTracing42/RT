ff05d92eb19ea628e54fbd98a8c8b16d965f5bb2
Joinacio <tokagehideto@free.fr>
2018-02-23 11:49:37 +0100

    Norme
    Norme sur loading_screen

--- src/loading_screen.c
+++ src/loading_screen.c
@@ -6,7 +6,7 @@
 /*   By: edescoin <edescoin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/30 09:27:44 by joinacio          #+#    #+#             */
-/*   Updated: 2018/02/23 11:32:59 by edescoin         ###   ########.fr       */
+/*   Updated: 2018/02/23 11:48:43 by joinacio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,9 @@ t_loadingbar	new_loading_bar(void)
 
 	lb = (t_loadingbar){NULL, 0, 0};
 	tmp = NULL;
-	if(!(tmp = SDL_LoadBMP("resources/loading.bmp")) ||
-		!(lb.loadingtexture = SDL_CreateTextureFromSurface(get_sdl_core()->renderer, tmp)))
+	if (!(tmp = SDL_LoadBMP("resources/loading.bmp")) ||
+		!(lb.loadingtexture = \
+			SDL_CreateTextureFromSurface(get_sdl_core()->renderer, tmp)))
 		exit_custom_error("rt : Erreur SDL2 : ", (char*)SDL_GetError());
 	SDL_FreeSurface(tmp);
 	return (lb);
@@ -43,7 +44,8 @@ void			update_loading_bar(t_loadingbar *lb)
 		dim = (SDL_Rect){0, WIN_HEIGHT - 20, WIN_WIDTH * p, 20};
 		SDL_SetRenderDrawColor(get_sdl_core()->renderer, 180, 180, 180, 255);
 		SDL_SetRenderTarget(get_sdl_core()->renderer, NULL);
-		SDL_RenderCopy(get_sdl_core()->renderer,lb->loadingtexture, NULL, NULL);
+		SDL_RenderCopy(get_sdl_core()->renderer, \
+				lb->loadingtexture, NULL, NULL);
 		SDL_RenderFillRect(get_sdl_core()->renderer, &dim);
 		SDL_RenderPresent(get_sdl_core()->renderer);
 		SDL_SetRenderTarget(get_sdl_core()->renderer, get_sdl_core()->target);
