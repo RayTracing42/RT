4ca3359849431f583eee89fbb42226f019849efd
Flavian CECILIEN <fcecilie@e2r8p10.42.fr>
2018-02-23 10:19:33 +0100

    mise a la norme libft

--- libraries/libft/sources/errors.c
+++ libraries/libft/sources/errors.c
@@ -6,7 +6,7 @@
 /*   By: edescoin <edescoin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/22 18:05:52 by edescoin          #+#    #+#             */
-/*   Updated: 2017/05/22 19:19:18 by edescoin         ###   ########.fr       */
+/*   Updated: 2018/02/23 10:13:34 by fcecilie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,15 @@
 
 void	exit_error(char *prog, char *err)
 {
-		ft_putstr_fd(prog, 2);
-		perror(err);
-		exit(1);
+	ft_putstr_fd(prog, 2);
+	perror(err);
+	exit(1);
 }
 
 void	exit_custom_error(char *prog, char *err)
 {
-		ft_putstr_fd(prog, 2);
-		ft_putstr_fd(err, 2);
-		write(2, "\n", 1);
-		exit(1);
+	ft_putstr_fd(prog, 2);
+	ft_putstr_fd(err, 2);
+	write(2, "\n", 1);
+	exit(1);
 }
--- libraries/libft/sources/garbage_collector.c
+++ libraries/libft/sources/garbage_collector.c
@@ -1,25 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   garbage_collector.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fcecilie <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/02/23 10:14:04 by fcecilie          #+#    #+#             */
+/*   Updated: 2018/02/23 10:17:52 by fcecilie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "garbage_collector.h"
 
-static t_gb_cell	**get_list()
+static t_gb_cell	**get_list(void)
 {
-	static t_gb_cell *list = NULL;
+	static t_gb_cell *list;
 
+	list = NULL;
 	return (&list);
 }
 
-void		*galloc(size_t size)
+void				*galloc(size_t size)
 {
 	t_gb_cell	**list;
 	t_gb_cell	*tmp;
 
 	tmp = (t_gb_cell*)malloc(sizeof(t_gb_cell));
 	tmp->data = malloc(size);
-	tmp->next = *(list = get_list());
+	list = get_list();
+	tmp->next = *(list);
 	*list = tmp;
 	return (tmp->data);
 }
 
-void		gfree(void *ptr)
+void				gfree(void *ptr)
 {
 	t_gb_cell	**list;
 	t_gb_cell	*tmp;
@@ -45,7 +59,7 @@ void		gfree(void *ptr)
 	free(ptr);
 }
 
-void		gexit(int status)
+void				gexit(int status)
 {
 	t_gb_cell	**list;
 	t_gb_cell	*tmp;
